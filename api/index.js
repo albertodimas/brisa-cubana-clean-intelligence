2763ab3129d7dcbd957629a9dbbe0eaab2763c84
albertodimas <albertodimasmorazaldivar@gmail.com>
2025-11-08 22:29:51 -0500

    fix(vercel): load api entrypoints without extra deps

--- api/index.js
+++ api/index.js
@@ -1,7 +1,4 @@
-import { Hono } from "hono";
 import app from "../apps/api/dist/index.js";
-
-void Hono;
 
 export const config = {
   runtime: "nodejs",
--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-import { Hono } from "hono";
 import app from "./apps/api/dist/index.js";
-
-void Hono;
 
 export default app;
